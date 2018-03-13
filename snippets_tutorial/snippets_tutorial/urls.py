5b29314c51a3b4c314ef5b0aea636d40e64d69db
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-13 08:35:01 -0300

    1. Serialization tutorial finished

--- snippets_tutorial/snippets_tutorial/urls.py
+++ snippets_tutorial/snippets_tutorial/urls.py
@@ -1,4 +1,4 @@
-"""tutorial URL Configuration
+"""snippets_tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -16,21 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from rest_framework import routers
-from tutorial.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(
-        r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')
-    ),
+    url(r'^', include('snippets.urls')),
     url(r'^admin/', admin.site.urls),
-
 ]
--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allow users to be viewed or deleted
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint tht allows groups to be viewed or deleted"
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
