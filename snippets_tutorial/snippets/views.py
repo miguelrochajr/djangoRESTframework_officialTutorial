4542403f27bc35f4f6204473da30df520f256be4
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-17 09:53:54 -0300

    Relationships and hyperlinked APIs

--- snippets_tutorial/snippets/views.py
+++ snippets_tutorial/snippets/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import (generics, status, permissions, renderers)
-from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
