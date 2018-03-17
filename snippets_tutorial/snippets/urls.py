43f0ff1818636959e1fd7d7f631757d1286ac746
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-17 10:26:58 -0300

    creating multiple views from each ViewSet class

--- snippets_tutorial/snippets/urls.py
+++ snippets_tutorial/snippets/urls.py
@@ -1,8 +1,31 @@
 from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
 
 from . import views
+
+
+snippet_list = views.SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = views.SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = views.SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = views.UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = views.UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 # API endpoints
 urlpatterns = [
