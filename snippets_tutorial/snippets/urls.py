647171bf3cc4e93c3fa7cb960b59d291e0b457db
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-17 12:50:46 -0300

    7-Schemas and client libs [FINISHED]

--- snippets_tutorial/snippets/urls.py
+++ snippets_tutorial/snippets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from . import views
 
@@ -9,8 +10,11 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 # API endpoints
 # The API URLs aew now determined automatically by the router.
 urlpatterns = [
+    url(r'^schemas/$', schema_view),
     url(r'^', include(router.urls))
 ]
