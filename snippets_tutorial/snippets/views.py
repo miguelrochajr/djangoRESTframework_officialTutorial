fd7a3874d1b550c208809c3d84e2098c453e51ab
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-17 09:52:18 -0300

    Refactoring to use ViewSets

--- snippets_tutorial/snippets/views.py
+++ snippets_tutorial/snippets/views.py
@@ -1,8 +1,7 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (api_view, detail_route)
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import (generics, status, permissions, renderers)
-from rest_framework.response import Response
+from rest_framework import (generics, permissions, renderers, viewsets)
 
 from django.contrib.auth.models import User
 
@@ -11,23 +10,10 @@ from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
-# List of HTTP status codes:
-# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides 'list' and 'detail' actions
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -56,57 +42,41 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     )
 
 
+# List of HTTP status codes:
+# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'users': reverse('users-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format),
     })
-
-
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
